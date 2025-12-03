e3c226c57be9aef949f0b1956d0b23915295da0c
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-12-03 11:57:31 +0500

    feat: add Home page and integrate react-router-dom for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import GsapFrom from './pages/GsapFrom'
 import GsapTo from './pages/GsapTo'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-    <GsapTo />
-    <GsapFrom />
+      <div className="bg-black min-h-screen w-full">
+        <Router>
+          <Routes>
+            <Route path="/gsapto" element={<GsapTo />} />
+            <Route path="/gsapfrom" element={<GsapFrom />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   )
 }
